fbeb573ca09e959df41e2eb01ba058de429430d6
TomiLakkakorpi <78793757+TomiLakkakorpi@users.noreply.github.com>
2022-11-25 15:31:45 +0200

    Worked on woodcutting
    Got basic functionality working. Still need to add an check if the player has an axe equipped before cutting tree. Also cant pick up cut logs yet.

--- SurvivalProject/Assets/Scripts/TreeScript.cs
+++ SurvivalProject/Assets/Scripts/TreeScript.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class TreeScript : MonoBehaviour
 {
     public Transform tree;
+    public bool isTreeMoved = false;
+    public bool isTreeHit = false;
+    public bool isPlayerInsideArea = false;
 
-    public Vector3 normalTreePosition = new Vector3(0f, 0f, 0f);
-    public Vector3 cutTreePosition = new Vector3(0f, 0f, 0f);
-    
-    public float moveSpeed = 5;
-    private bool treeMoved = false;
+    public GameObject TestLog;
+
+    [SerializeField] private Transform Log1SpawnPoint;
+    [SerializeField] private Transform Log2SpawnPoint;
+    [SerializeField] private Transform Log3SpawnPoint;
 
     void Start()
     {
@@ -19,29 +22,71 @@ public class TreeScript : MonoBehaviour
 
     void Update()
     {
-        if(treeMoved)
+        //Check if button has been pressed
+        if(Input.GetKeyDown(KeyCode.R))
         {
-            tree.position = Vector3.Lerp(tree.position, cutTreePosition, Time.deltaTime * moveSpeed);
-        }
-        else
-        {
-            tree.position = Vector3.Lerp(tree.position, normalTreePosition, Time.deltaTime * moveSpeed);
+            //Check if player is near the tree
+            if (isPlayerInsideArea == true) 
+            {
+        
+            tree.Translate(0, -10, 0);
+            isTreeMoved = true;
+
+            //Start coroutine and call time delay
+            StartCoroutine(TimeDelay());
+            TimeDelay();
+            isTreeMoved = false;
+
+            //Get a random value between 1 and 3    
+            int numberOfLogs = Random.Range(1,4);
+
+            //Spawn logs based on the random value
+            if (numberOfLogs == 1)
+            {
+                Instantiate(TestLog, Log1SpawnPoint.position, Log1SpawnPoint.rotation);
+            }
+
+            if (numberOfLogs == 2)
+            {
+                Instantiate(TestLog, Log1SpawnPoint.position, Log1SpawnPoint.rotation);
+                Instantiate(TestLog, Log2SpawnPoint.position, Log2SpawnPoint.rotation);
+            }
+
+            if (numberOfLogs == 3)
+            {
+                Instantiate(TestLog, Log1SpawnPoint.position, Log1SpawnPoint.rotation);
+                Instantiate(TestLog, Log2SpawnPoint.position, Log2SpawnPoint.rotation);
+                Instantiate(TestLog, Log3SpawnPoint.position, Log3SpawnPoint.rotation);
+            } 
+
+            isTreeHit = false;
+            }
         }
     }
 
+    //Turn "isPlayerInsideArea" to true when player enters the tree´s vicinity
     private void OnTriggerEnter(Collider other) 
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player") 
         {
-            treeMoved = true;
+            isPlayerInsideArea = true;
         }
     }
 
+    //Turn "isPlayerInsideArea" to false when player exits the tree´s vicinity
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+      if (other.tag == "Player")
         {
-            treeMoved = false;
+            isPlayerInsideArea = false;
         }
+    }
+
+    // Function for tree respawn delay
+    IEnumerator TimeDelay()
+    {
+        //Adding the timer and after the timer tree will be moved back to its original position
+        yield return new WaitForSeconds(5);
+        tree.Translate(0, 10, 0);
     }
 }
