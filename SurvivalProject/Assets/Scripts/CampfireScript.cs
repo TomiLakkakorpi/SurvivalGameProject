e608f555f29f897c63e48ad06f8d58004042e60d
TomiLakkakorpi <78793757+TomiLakkakorpi@users.noreply.github.com>
2022-12-09 15:52:56 +0200

    Continued campfire mechanic

--- SurvivalProject/Assets/Scripts/CampfireScript.cs
+++ SurvivalProject/Assets/Scripts/CampfireScript.cs
@@ -8,11 +8,17 @@ public class CampfireScript : MonoBehaviour
     public Transform logs;
     public Transform fire;
 
+    public HUD Hud;
+
     public bool isPlayerNearCampfire = false;
 
     public bool areRocksPlaced = false;
     public bool areLogsPlaced = false;
     public bool isFireActive = false;
+
+    public int timesClickedRocks;
+    public int timesClickedLogs;
+    public int timesClickedFire;
 
     void Start()
     {
@@ -22,21 +28,58 @@ public class CampfireScript : MonoBehaviour
     }
 
     void Update()
-    {
-        if(isPlayerNearCampfire == true && Input.GetKeyDown(KeyCode.T) && areRocksPlaced == false) /* && if player has 9 rocks in inventory*/
+    {      
+        if(isPlayerNearCampfire == true && areRocksPlaced == false && Input.GetKeyDown(KeyCode.T) /* add check here to see if player has 9 rocks in hotbar */ )
         {
-            moveRocks();            
+            timesClickedRocks++;
+
+            if(timesClickedRocks == 1)
+            {
+                StartCoroutine(moveRocks());
+                moveRocks();
+            }
         }
 
-        if(isPlayerNearCampfire == true && Input.GetKeyDown(KeyCode.T) && areLogsPlaced == false) /* && if player has 3 logs in inventory*/
+        if(isPlayerNearCampfire == true && areRocksPlaced == true && areLogsPlaced == false && Input.GetKeyDown(KeyCode.T) /* add check here to see if player has 3 logs in hotbar */)
         {
-            moveLogs();
+            timesClickedLogs++;
+
+            if (timesClickedLogs == 1)
+            {
+                StartCoroutine(moveLogs());
+                moveLogs();
+            }
         }
 
-        if(isPlayerNearCampfire == true && areRocksPlaced == true && areLogsPlaced == true && Input.GetKeyDown(KeyCode.Y) && isFireActive == false)
+        if(isPlayerNearCampfire == true && areRocksPlaced == true && areLogsPlaced == true && isFireActive == false && Input.GetKeyDown(KeyCode.T))
+        { 
+            timesClickedFire++;
+
+            if(timesClickedFire == 1)
+            {
+                StartCoroutine(moveFire());
+                moveFire();
+            }
+        }
+
+        if(isPlayerNearCampfire == true && areRocksPlaced == false && areLogsPlaced == false)
         {
-            StartCoroutine(moveFire());
-            moveFire();
+            Hud.OpenMessagePanel("Press -T- to place the rocks");
+        }
+
+        if(isPlayerNearCampfire == true && areRocksPlaced == true && areLogsPlaced == false)
+        {
+            Hud.OpenMessagePanel("Press -T- to place the logs");
+        }
+
+        if(isPlayerNearCampfire == true && areRocksPlaced == true && areLogsPlaced == true && isFireActive == false)
+        {
+            Hud.OpenMessagePanel("Press -T- to light the campfire");
+        }
+
+        if(areRocksPlaced == true && areLogsPlaced == true && isFireActive == true)
+        {
+            Hud.CloseMessagePanel();
         }
     }
 
@@ -53,31 +96,42 @@ public class CampfireScript : MonoBehaviour
       if (other.tag == "Player")
         {
             isPlayerNearCampfire = false;
+            Hud.CloseMessagePanel();
         }
     }
 
-    void moveRocks()
+    IEnumerator moveRocks()
     {
         rocks.Translate(0, 10, 0);
+        Hud.CloseMessagePanel();
+        yield return new WaitForSeconds(0.5F);
         areRocksPlaced = true;
+        timesClickedRocks = 0;
     }
 
-    void moveLogs()
+    IEnumerator moveLogs()
     {
         logs.Translate(0, 10, 0);
-        areLogsPlaced = true;
+        Hud.CloseMessagePanel();
+        yield return new WaitForSeconds(0.5F);
+        areLogsPlaced = true; 
+        timesClickedLogs = 0;
     }
 
     IEnumerator moveFire()
     {
+        int respawnTime = Random.Range(2, 10);
+
         fire.Translate(0, 10, 0);
+        Hud.CloseMessagePanel();
         isFireActive = true;
 
-        yield return new WaitForSeconds(60F);
+        yield return new WaitForSeconds(respawnTime);
         fire.Translate(0, -10, 0);
         isFireActive = false;
 
         logs.Translate(0, -10, 0);
         areLogsPlaced = false;
+        timesClickedFire = 0;
     }
 }
