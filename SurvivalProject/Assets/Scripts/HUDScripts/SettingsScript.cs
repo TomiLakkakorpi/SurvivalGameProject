a5cc7f9c86f033da62c64e43d95f0b3b458e3d31
Tuomas Riihiaho <78533799+c0ritu00@users.noreply.github.com>
2022-12-09 11:10:47 +0200

    Sensitivity Slider working

--- SurvivalProject/Assets/Scripts/HUDScripts/SettingsScript.cs
+++ SurvivalProject/Assets/Scripts/HUDScripts/SettingsScript.cs
@@ -9,15 +9,15 @@ public class SettingsScript : MonoBehaviour
 
     public CinemachineFreeLook freeLookCam;
 
-    //CinemachineOrbitalTransposer xAxis;
+    
 
     private Slider sensitivitySlider = GameObject.Find("SensitivitySlider").GetComponent<Slider>();
-    //private Panel sensitivityDisplay = GameObject.Find("SensitivityPanel").GetComponent<Panel>();
+    
 
 
     void update()
     {
-        //sensitivityDisplay = sensitivitySlider.value;
+        
     }
     
     public void sliderValue()
--- SurvivalProject/Assets/Scripts/HUDScripts/SliderValueText.cs
+++ SurvivalProject/Assets/Scripts/HUDScripts/SliderValueText.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Cinemachine;
 
 public class SliderValueText : MonoBehaviour
 {
     private Slider slider;
     private Text textComp;
+    public CinemachineFreeLook freeLookCam;
 
     void Awake()
     {
@@ -16,10 +18,15 @@ public class SliderValueText : MonoBehaviour
     {
         UpdateText(slider.value);
         slider.onValueChanged.AddListener(UpdateText);
+        
+        
     }
 
-    void UpdateText(float val)
+    public void UpdateText(float val)
     {
         textComp.text = slider.value.ToString();
+        freeLookCam.m_YAxis.m_MaxSpeed = slider.value;
+        freeLookCam.m_XAxis.m_MaxSpeed = (slider.value + 1) * 100;
+        
     }
 }
